7d99747b6158640fc5f8df521bfc68c26ff4ed6c
Lopez-CL <cllopez511@gmail.com>
2022-08-24 19:43:04 -0400

    Figured out how to close gaps Pacman in world

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 var world = [
-[2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
-[2, 1, 1, 2, 1, 1, 1, 1, 1, 2],
-[2, 1, 1, 2, 1, 1, 1, 1, 1, 2],
-[2, 1, 1, 1, 2, 1, 1, 1, 1, 2],
-[2, 1, 1, 1, 2, 1, 1, 1, 1, 2],
-[2, 1, 1, 1, 2, 1, 1, 1, 1, 2],
-[2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-[2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-[2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-[2, 2, 2, 2, 2, 2, 2, 2, 2, 2]];
+    [2,2,2,2,2,2,2,2,2,2], 
+    [2,1,2,1,1,1,1,1,1,2],
+    [2,1,2,1,1,1,1,1,1,2],
+    [2,1,2,1,1,1,1,1,1,2],
+    [2,1,2,2,2,2,1,1,1,2],
+    [2,1,1,1,1,2,2,1,1,2],
+    [2,1,1,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,1,1,2],
+    [2,2,2,2,2,2,2,2,2,2]
+];
 
 function renderWorld(){
     var worldBlocks = '';
     for (i = 0; i < world.length; i++){
-        worldBlocks += '\n<div class="row">\n';
-        for (var j = 0; j < world.length; j++)
-            if (world[i][j] == 2){
-                worldBlocks += '<div class="brick"></div>'; //pick up at 23"39//
-            }
-            else if (world[i][j] == 1){
-                worldBlocks += '<div class="coin"></div>';
-            }
-            if (world[i][j] == 0){
-                worldBlocks += '<div class="empty"></div>';
-            }
-        worldBlocks += '\n</div>';
+        worldBlocks += "<div class='row'>";
+        for (var j = 0; j < world[i].length; j++){
+            if(world[i][j] == 2)
+                worldBlocks += "<div class='brick'></div>";
+            else if(world[i][j] == 1)
+                worldBlocks += "<div class='coin'></div>";
+            if(world[i][j] == 0)
+                worldBlocks += "<div class='empty'></div>";
+        }
+        worldBlocks += "</div>";
         console.log( worldBlocks);
     }
     document.getElementById("world").innerHTML = worldBlocks;
